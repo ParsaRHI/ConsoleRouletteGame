0d3a02d0cb7187fb0fd460ad7b2818a453e017c6
Parsa <116366001+ParsaRHI@users.noreply.github.com>
2024-09-05 14:54:04 -0400

    save

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/ParsaRH/CLionProjects/ConsoleGame
+# Install script for directory: C:/Users/ParsaRH/CLionProjects/ConsoleGame/ConsoleRouletteGame
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/ParsaRH/CLionProjects/ConsoleGame/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/ParsaRH/CLionProjects/ConsoleGame/ConsoleRouletteGame/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
